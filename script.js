857a2c497c9c6263812147f24f2c8f6da566ae47
iamlearner0 <quadrisaheed001@gmail.com>
2024-03-20 23:30:59 +0100

    new update

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ eventListener();
 function eventListener() {
   emailInput.addEventListener("keyup", checkEmail);
   emailInput.addEventListener("blur", checkEmail);
-  // submitBtn.addEventListener("click", checkEmail);
+  submitBtn.addEventListener("click", submitForm);
 }
 
 // To check if the inputted mail is correct and if it's more than 5 in length
@@ -20,6 +20,18 @@ function checkEmail() {
     errorMessage.classList.remove("error");
     errorIcon.classList.remove("icon");
     emailInput.style.border = "2px solid hsl(0, 36%, 70%)";
+  } else {
+    errorMessage.classList.add("error");
+    errorIcon.classList.add("icon");
+    emailInput.style.border = "1px solid red";
+  }
+}
+
+function submitForm() {
+  let emailInputValue = emailInput.value;
+  let emailFormat = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailFormat.test(emailInputValue) || emailInputValue > 5) {
+    form.submit();
   } else {
     errorMessage.classList.add("error");
     errorIcon.classList.add("icon");
